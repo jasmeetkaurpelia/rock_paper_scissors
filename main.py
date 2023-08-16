dfd45d9e6f0748052c82a5a33a110791892a36e2
Jasmeet Kaur <130532281+jasmeetkaurpelia@users.noreply.github.com>
2023-08-16 22:03:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 from random import randint
-t = [rock, paper, scissors]
+t = ['rock', 'paper', 'scissors']
 computer = t[randint(0,2)]
 player = False
 while player==False:
-  player = input("Rock, Paper, Scissor?").lower()
-  if player == computer:
-    print("Tie!")
-  elif player == "rock":
-    if computer == "paper":
-      print("You lose!, Computer wins.")
+    player = input("Rock, Paper, Scissor?\n").lower()
+    if player == computer:
+        print("Tie!")
+    elif player == "rock":
+        if computer == "paper":
+            print("You lose!, Computer wins.")
+        else:
+            print("Congrats! You win.")
+    elif player == "paper":
+        if computer == "scissor":
+            print("You lose!, Computer wins.")
+        else:
+            print("Congrats! You win.")
+    elif player == "scissor":
+        if computer == "rock":
+            print("You lose!, Computer wins.")
+        else:
+            print("Congrats! You win.")
     else:
-      print("Congrats! You win.")
-  elif player == "paper":
-    if computer == "scissor":
-      print("You lose!, Computer wins.")
-    else:
-      print("Congrats! You win.")
-  elif player == "scissor":
-    if computer == "rock":
-      print("You lose!, Computer wins.")
-    else:
-      print("Congrats! You win.")
-  else:
-    print("Wrong input! Check spellings.")
-  player = False
+        print("Wrong input! Check spellings.")
+    player = False
     computer = t[randint(0,2)]
